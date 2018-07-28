860d4a641ee8bc748db93f0d1ff62372f4fb9548
Danny Leviev <danny.leviev@gmail.com>
2018-07-28 09:46:31 +0300

    Project skeleton

---
+++ src/main/java/com/mynextcomp/DdosProtector/DdosProtectorApplication.java
@@ -0,0 +1,12 @@
+package com.mynextcomp.DdosProtector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdosProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdosProtectorApplication.class, args);
+	}
+}
